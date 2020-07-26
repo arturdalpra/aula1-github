fdc49587e1f605e2f0c7be2d1633d49220ef710c
arturdalpra <arturdalpra@gmail.com>
2020-07-26 19:48:27 -0300

    mensagem de boa noite

--- src/application/PrograminhaInicial.java
+++ src/application/PrograminhaInicial.java
@@ -3,10 +3,9 @@ package application;
 public class PrograminhaInicial {
 
 	public static void main(String[] args) {
-	
 		System.out.println("bom dia");
 		System.out.println("boa tarde");
-
+		System.out.println("boa noite");
 	}
 
 }
